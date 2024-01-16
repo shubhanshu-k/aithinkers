0dc65e8b85e02e9031a1c6ec5a5e30f9cadb3dee
Shubhanshu Kakkar <84907458+shubhanshu-k@users.noreply.github.com>
2024-01-17 03:28:52 +0530

    Add files via upload
    code to implement try with multiple catch blocks and how they are executed

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int i,j,k=0;
+      int a[]= new int [4];
+       i=8;
+       j=2;
+       
+       try {
+         k=i/j;
+         for(int c=0;  c<=4; c++){
+           a[c]=c+1;
+         }
+         
+         for ( int value : a){
+           System.out.println(value);
+         }
+       }
+       
+       catch (ArithmeticException e){
+         System.out.println("cannot be divided by zero"+e);
+  
+       }
+       
+       catch (ArrayIndexOutOfBoundsException e){
+         System.out.println("cant store more than 4 elements in the array "+e);
+       }
+}
+}
